3b02215686120d96c348025de96b3f126cbeeed6
Carlos <carlosvini.amaral@gmail.com>
2021-08-17 18:07:24 -0300

    Versão 1.3
    Elementos filhos e pai na mesma cor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,14 +11,20 @@ class HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
+    var container = Container(
+      height: 150,
+      width: 150,
+      color: Colors.red,
+      child: Container(
+        height: 50,
+        width: 50,
+        color: Colors.green,
+      ),
+    );
     return Scaffold(
       //
       appBar: AppBar(title: Text('App Flutter ADS')),
-      body: Container(
-        height: 50,
-        width: 50,
-        color: Colors.red,
-      ),
+      body: container,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_circle),
         onPressed: () {
